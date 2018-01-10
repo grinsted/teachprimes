8f6791c460655794fc0024533b237f5327d12f10
Aslak Grinsted <ag+github@glaciology.net>
2018-01-10 13:11:55 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+### Some code to for teaching primes 
+
+This code generates figures for teaching multiplication, division and primes. 
+
+It is heavily inspired by the board game prime climb.  [Recommended](http://primeclimbgame.com/)
+
+
---
+++ teachprimes.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 11:06:33 2018
+
+
+Code for making figures similar to those used in the board game "Prime Climb". 
+
+
+
+
+@author: aslak
+"""
+
+
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy
+import sympy
+
+primecolors={
+ 2: 'xkcd:red',
+ 3: 'xkcd:blue',
+ 5: 'xkcd:green',
+ 7: 'xkcd:pink',
+ 11: 'xkcd:brown',
+ 13: 'xkcd:purple',
+ 17: 'xkcd:light blue',
+ 19: 'xkcd:teal',
+ 23: 'xkcd:orange',
+ 29: 'xkcd:magenta',
+ 31: 'xkcd:yellow',
+ 37: 'xkcd:lime green',
+ 41: 'xkcd:violet',
+ 43: 'xkcd:dark green',
+ 47: 'xkcd:cyan'}
+
+def drawnumber(xo,yo,num):
+    if num == 1:
+        factors = [1]
+    else:
+        factors = sympy.factorint(num)
+        factors = list(sympy.utilities.iterables.multiset_combinations(factors, sum(factors.values())))[0]
+
+    for ix,f in enumerate(factors):
+        theta1 = 360*ix/len(factors)
+        theta2 = 360*(ix+1)/len(factors)
+        color = primecolors.get(f)
+        if not color: 
+            color = 'xkcd:grey'
+        p = patches.Wedge(center=(xo,yo), r=0.5, theta1=0, theta2=360, 
+                          edgecolor="none", facecolor="#000000", linewidth=0);
+        plt.gca().add_patch(p)
+        p = patches.Wedge(center=(xo,yo), r=1.0, theta1=theta1, theta2=theta2, width=0.5, 
+                          edgecolor="#000000", facecolor=color, linewidth=2)
+        plt.gca().add_patch(p)
+        
+        plt.text(xo,yo, "{}".format(num),horizontalalignment='center',verticalalignment='center',color="#FFFFFF")          
+
+
+def spiral():
+    jmax = 101
+    
+    afun = lambda jj: numpy.sqrt(((jmax+1-jj)/jmax)) #angle
+    rfun = lambda jj: afun(jj)*14.0 #radius
+    xfun = lambda jj: numpy.multiply(rfun(jj), numpy.cos(afun(jj)*11.0*numpy.pi))
+    yfun = lambda jj: numpy.multiply(rfun(jj), numpy.sin(afun(jj)*11.0*numpy.pi))
+    
+    jj = numpy.linspace(1,jmax,2000)
+    plt.plot(xfun(jj),yfun(jj), 'k')
+    for jj in numpy.arange(1,jmax+1):
+        drawnumber(xfun(jj),yfun(jj),jj)
+    
+
+def square(n):
+    jmax = n*n
+    
+    xfun = lambda jj: ((jj-1) % 10)*2.2
+    yfun = lambda jj: numpy.floor((jj-1)/10.0)*2.2
+    
+    for jj in numpy.arange(1,jmax+1):
+        drawnumber(xfun(jj),yfun(jj),jj)
+
+
+fig = plt.figure(facecolor='black')
+ax1 = plt.Axes(fig, [0., 0., 1., 1.])
+ax1.set_axis_off()
+fig.add_axes(ax1)
+ax1.axis('off')
+
+square(10)
+
+
+ax1.get_xaxis().set_visible(False)
+ax1.get_yaxis().set_visible(False)
+ax1.axis('tight')
+ax1.axis('equal')
+plt.show()
+
